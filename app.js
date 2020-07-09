55e37ed3bde90e94209d15bfa237270a2bb093e1
Joaquin Fox <Joaquin.Fox@gmail.com>
2020-07-08 20:48:06 -0700

    about and tech pages

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ express()
   .get('/', (req, res) => {
     res.render('index');
   })
+  .get('/about', (req, res) => {
+    res.render('about');
+  })
+  .get('/tech', (req, res) => {
+    res.render('tech');
+  })
   .listen(PORT || 3000, () => {
     console.log(`Listening on ${PORT || 3000}`);
   });

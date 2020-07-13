7b6c57f2552fab98116cc9f05f007c11a22b9756
Joaquin Fox <Joaquin.Fox@gmail.com>
2020-07-12 18:28:22 -0700

    gallery, about views and styles, styles modularized

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ express()
   .set('view engine', 'ejs')
   .use(express.static('public'))
   .get('/', (req, res) => {
-    res.render('index');
+    res.render('landing');
   })
   .get('/about', (req, res) => {
     res.render('about');
   })
   .get('/tech', (req, res) => {
     res.render('tech');
+  })
+  .get('/gallery', (req, res) => {
+    res.render('gallery');
   })
   .listen(PORT || 3000, () => {
     console.log(`Listening on ${PORT || 3000}`);
